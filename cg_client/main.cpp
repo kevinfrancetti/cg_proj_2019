8c271f6848d2d83c803645fccecdcf5fb1d4ac40
kevinfrancetti <kevin.francetti91@gmail.com>
2019-10-08 20:12:41 +0200

    mmmm

---
+++ cg_client/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: kevin
+ *
+ * Created on October 8, 2019, 7:56 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+	cout << "ola" << endl;
+    return 0;
+}
+
