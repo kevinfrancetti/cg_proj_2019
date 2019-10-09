af33bbf6dde023fff0661196fbee27cb172ea086
kevinfrancetti <kevin.francetti91@gmail.com>
2019-10-09 12:17:10 +0200

    CODIO CHE ODIO QUESTI IDE DI MERDAgit status git status !

--- cg_engine/class01.cpp
+++ cg_engine/class01.cpp
@@ -10,10 +10,11 @@ using namespace std;
 LIB_API Class01::Class01() {
 	cout << "class constructor miao" << endl;
 	glm::vec4 Position = glm::vec4(glm::vec3(0.0), 1.0);
+	cout << "1" << endl;
 	glm::mat4 Model = glm::mat4(1.0);
-	Model[4] = glm::vec4(1.0, 1.0, 0.0, 1.0);
-	glm::vec4 Transformed = Model * Position;
-	cout << glm::to_string(Transformed) << endl;
+	cout << "2" << endl;
+	cout << "3" << endl;
+	cout << glm::to_string(Position) << endl;
 }
 
 
