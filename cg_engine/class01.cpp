104203bb7f25159eabc6b88dc431e533856cde0d
Scrotuss <scroto@protonmail.ch>
2019-10-09 11:35:48 +0200

    add glm, and replace all LF with CRLF in everi hpp file iside glm

--- cg_engine/class01.cpp
+++ cg_engine/class01.cpp
@@ -1,12 +1,19 @@
 #include "class01.h"
 
 #include <iostream>
+#include "glm/glm.hpp"
+#include "glm/gtx/string_cast.hpp"
 
 
 using namespace std;
 
 LIB_API Class01::Class01() {
 	cout << "class constructor miao" << endl;
+	glm::vec4 Position = glm::vec4(glm::vec3(0.0), 1.0);
+	glm::mat4 Model = glm::mat4(1.0);
+	Model[4] = glm::vec4(1.0, 1.0, 0.0, 1.0);
+	glm::vec4 Transformed = Model * Position;
+	cout << glm::to_string(Transformed) << endl;
 }
 
 
